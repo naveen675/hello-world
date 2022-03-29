0c910231b226ca4be01e111ddc6d993fc1e06402
unknown <spnaveen675@gmail.com>
2022-03-29 18:52:22 +0530

    added public file

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(express.static('static'));
+app.use(express.static('public'));
+app.use(express.static('static'));
+
 
 app.get('/', (req, res) => {
   
