b7cfdca54628a58c9c3592cead2e6275cffeb0d2
unknown <spnaveen675@gmail.com>
2022-03-29 18:47:19 +0530

    added static file

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use(express.static('static'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  
+  res.send("hello");
 });
+
+app.get("/index",(req,res) => {
+  res.send("Index");
+})
+
 
 app.listen(port, () => {
   console.log(`Server listening at port: ${port}`)
 });
+
+
+
+
 
