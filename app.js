6f39c13600c5d3a4504ff01b60f2562acf9c06d2
unknown <spnaveen675@gmail.com>
2022-04-07 11:49:57 +0530

    added fetching data through API

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-//const fetch = require('node-fetch');
+const fetch = require('node-fetch');
 
 app.use(cors());
 
@@ -14,24 +14,18 @@ const logger =  (req,res,next) => {
 
 
 app.use(logger);
-app.get('/github', function (req, res) {
+let data = app.get('/github', function (req, res) {
 
   const axios = require('axios')
 
-  axios
-    .get('https://api.github.com/users/naveen675/repos')
-    .then(response => {
-      console.log(`statusCode: ${response.status}`)
-      res.send(response.data);
+  fetch('https://api.github.com/users/naveen675/repos').then(
+    (response) => { return response.json();
+    }).then(
+      (data) => {Object.entries(data).forEach(([key, value]) => {
+        console.log(`${key} ${value["name"]}`);
     })
-    .catch(error => {
-      console.error(error)
-    })
-
-    
- 
+  })
 });
-
 
 
 app.get("/users/",function (req,res,next) {
